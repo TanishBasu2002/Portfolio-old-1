ad055dbb8b98e2475b659fb6dcc9209c42ab778b
Tanish2002Basu <>
2023-03-26 12:20:39 +0530

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "gray-20": "#ebf4f8",
+        "gray-50": "#e6ebef",
+        "gray-100": "#ccd3df",
+        "gray-500": "#000b5e",
+        "primary-100": "#e0e1ff",
+        "primary-300": "#a5a3ff",
+        "primary-500": "#6687ff",
+        "secondary-400": "#5bdeff",
+        "secondary-500": "#32ccff",
+      },
+      backgroundImage: (theme) => ({
+        "gradient-yellowred":
+          "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)",
+        "mobile-home": "url('./assets/HomePageGraphic.png')",
+      }),
+      fontFamily: {
+        dmsans: ["DM Sans", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      content: {
+        evolvetext: "url('./assets/EvolveText.png')",
+        abstractwaves: "url('./assets/AbstractWaves.png')",
+        sparkles: "url('./assets/Sparkles.png')",
+        circles: "url('./assets/Circles.png')",
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
+    },
+  },
+  plugins: [],
+};
